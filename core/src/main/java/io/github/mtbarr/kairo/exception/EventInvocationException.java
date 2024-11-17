bb4a33363b0d87c1f7cee2f2d65b2b3634f9c905
mtbarr <mawbarr@gmail.com>
2024-11-17 15:33:00 -0300

    fix: make `EventInvocationException` extends runtime RuntimeException

--- core/src/main/java/io/github/mtbarr/kairo/exception/EventInvocationException.java
+++ core/src/main/java/io/github/mtbarr/kairo/exception/EventInvocationException.java
@@ -1,6 +1,6 @@
 package io.github.mtbarr.kairo.exception;
 
-public class EventInvocationException extends Error {
+public class EventInvocationException extends RuntimeException {
 
   public EventInvocationException(Throwable cause) {
     super(cause);
